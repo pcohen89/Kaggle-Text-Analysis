6f0849b9dd5f7091a0aa1c7127cf1736ce51446e
unknown <p_cohen@GG-PF00HKY1-L.GreatestGood.com>
2014-06-04 16:29:40 -0500

    cleaning code and creating submission

--- Cleaning essays in python.py
+++ Cleaning essays in python.py
@@ -36,27 +36,20 @@ outcomes.count()
 outcomes.is_exciting[outcomes.is_exciting=='t'] = 1
 outcomes.is_exciting[outcomes.is_exciting=='f'] = 0
 
-# merge with essays
+# merge with essays to create train data
 
-essays_w_outcomes = essays.merge(outcomes, on='projectid')
-#Note we only have outcomes for a subset of the essays 619000/640000
-print(essays_w_outcomes['projectid'].count())
-
-#keep only where essay and outcomes is filled (unused)
-index_nomiss_exciting = pd.notnull(essays_w_outcomes.is_exciting) 
-index_nomiss_essay = pd.notnull(essays_w_outcomes.essay)
-essays_w_outcomes = essays_w_outcomes[index_nomiss_essay & index_nomiss_exciting]
-essays_w_outcomes[index_nomiss_essay & index_nomiss_exciting]
-
-#Create some basic variables that could be interesting
-
-essays_w_outcomes['essay_len'] = [len(cell) for cell in essays_w_outcomes.essay]
-essays_w_outcomes['is_essay_long'] = [len(cell)>2000 for cell in essays_w_outcomes.essay]
+unsplit = essays.merge(outcomes, how='outer', on='projectid')
+print(unsplit['projectid'].count())
+test = unsplit[pd.isnull(unsplit.is_exciting)]
+train = unsplit[pd.notnull(unsplit.is_exciting)]
 
 #use a vectorizer to count word usage instances
 
 vectorizer = TfidfVectorizer(min_df=1)
-X = vectorizer.fit_transform(essays_w_outcomes.essay)
+X = vectorizer.fit(train.essay)
+train_tokens = vectorizer.transform(train.essay)
+test_tokens = vectorizer.transform(test.essay)
+
 
 # when we are really ready to do this seriously, fit transformation on train only
 # then transform the whole thing
@@ -66,25 +59,27 @@ X = vectorizer.fit_transform(essays_w_outcomes.essay)
 
 def pc_ridge(penalty):
     ridge= RidgeCV(alphas= penalty, store_cv_values=True, normalize=True)
-    ridge.fit(X[0:documents], essays_w_outcomes.is_exciting[0:documents])
-    predictions = ridge.predict(X)
-    rmse = np.sqrt(np.mean((essays_w_outcomes.is_exciting-predictions)**2))
+    ridge.fit(train_tokens[0:documents], train.is_exciting[0:documents])
+    predictions = ridge.predict(train_tokens)
+    rmse = np.sqrt(np.mean((train.is_exciting-predictions)**2))
     return rmse
     
-documents = 20000
+documents = 25000
 
 t0= time.time()
 optimizer = minimize(pc_ridge, array([15]), method='nelder-mead', options= {'xtol':1e-2, 'disp':True})
 print "It took {time} minutes to optimize".format(time=(time.time()-t0)/60)
 
-
+t0= time.time()
 ridge= RidgeCV(alphas=optimizer.x, store_cv_values=True, normalize=True)
-ridge.fit(X[0:documents], essays_w_outcomes.is_exciting[0:documents])  
-predictions = ridge.predict(X)  
-rmse = np.sqrt(np.mean((essays_w_outcomes.is_exciting-predictions)**2))
+ridge.fit(train_tokens[0:documents], train.is_exciting[0:documents])
+print "It took {time} minutes to run the optimized ridge".format(time=(time.time()-t0)/60)  
+predictions = ridge.predict(train_tokens)  
+rmse = np.sqrt(np.mean((train.is_exciting-predictions)**2))
 rmse
 
-
+entry = pd.DataFrame(data=test['projectid']) 
+entry['predictions'] = ridge.predict(test_tokens)
 
 
 
