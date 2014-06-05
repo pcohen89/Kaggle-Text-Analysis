7ce8b8588e348aac815ab6b6d3121c841381c9c2
unknown <p_cohen@GG-PF00HKY1-L.GreatestGood.com>
2014-06-05 17:56:43 -0500

    creating submissions and ensemble with length

--- Cleaning essays in python.py
+++ Cleaning essays in python.py
@@ -13,7 +13,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model.ridge import RidgeCV
 import numpy as np
 import time
-import statsmodels.api as sm
+import statsmodels as sm
 from scipy.optimize import minimize
 
 
@@ -43,16 +43,17 @@ print(unsplit['projectid'].count())
 test = unsplit[pd.isnull(unsplit.is_exciting)]
 train = unsplit[pd.notnull(unsplit.is_exciting)]
 
+# create a word count variable
+
+train['word_count'] = [len(cell) for cell in train.essay]
+test['word_count'] = [len(cell) for cell in test.essay]
+
 #use a vectorizer to count word usage instances
 
 vectorizer = TfidfVectorizer(min_df=1)
 X = vectorizer.fit(train.essay)
 train_tokens = vectorizer.transform(train.essay)
 test_tokens = vectorizer.transform(test.essay)
-
-
-# when we are really ready to do this seriously, fit transformation on train only
-# then transform the whole thing
 
 
 # use optimizer to find best penalty for ridge
@@ -70,6 +71,8 @@ t0= time.time()
 optimizer = minimize(pc_ridge, array([15]), method='nelder-mead', options= {'xtol':1e-2, 'disp':True})
 print "It took {time} minutes to optimize".format(time=(time.time()-t0)/60)
 
+# run ridge with optimal penalization
+
 t0= time.time()
 ridge= RidgeCV(alphas=optimizer.x, store_cv_values=True, normalize=True)
 ridge.fit(train_tokens[0:documents], train.is_exciting[0:documents])
@@ -78,9 +81,30 @@ predictions = ridge.predict(train_tokens)
 rmse = np.sqrt(np.mean((train.is_exciting-predictions)**2))
 rmse
 
-entry = pd.DataFrame(data=test['projectid']) 
-entry['predictions'] = ridge.predict(test_tokens)
+# create an OLS regression for word count
+ols= sm.regression.linear_model.OLS(train.is_exciting, train.word_count)
+results= ols.fit()
 
+# add ols and ridge predictions to train and test
+
+train['ridge_predictions']=ridge.predict(train_tokens) 
+train['length_predictions'] = train.word_count*results.params[0]
+test['ridge_predictions']=ridge.predict(test_tokens) 
+test['length_predictions'] = test.word_count*results.params[0]
+
+data_for_ensemble['ridge_predictions'] = pd.DataFrame(train.ridge_predictions)
+data_for_ensemble['length_predictions'] = pd.DataFrame(train.length_predictions)
+
+# create a ridge regression that incorporates the bag of words and essay length
+ridge= RidgeCV(alphas=1, store_cv_values=True, normalize=True)
+ridge.fit(train.ridge_predictions, train.is_exciting)
+print "It took {time} minutes to run the optimized ridge".format(time=(
+
+
+#create an entry
+entry = pd.DataFrame(data=test['projectid']) 
+entry['is_exciting'] = ridge.predict(test_tokens)
+entry.to_csv("S:/General/Training/Ongoing Professional Development/Kaggle/Predicting Excitement at DonorsChoose/Data/Submissions/6.5.2014 PC submission.csv", index=False)
 
 
 # extra code snippets
